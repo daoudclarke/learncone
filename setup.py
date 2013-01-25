025ed9a5773ebedfdbf3307be62deafb8a7a3966
Daoud Clarke <daoud.clarke@gmail.com>
2013-01-25 09:11:38 +0000

    Add setup.py and restructure directories

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='learncone',
+      version='0.1',
+      description='Machine learning classification using cones for SciKit learn',
+      #url='http://github.com/storborg/funniest',
+      author='Daoud Clarke',
+      #author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['learncone'],
+      #zip_safe=False
+      )
