dc007c1fdd5fceacde32b62394dc0831efdf506b
Daoud Clarke <daoud.clarke@gmail.com>
2013-07-12 09:19:40 +0100

    Split up fit function

--- learncone/ConeEstimatorSVM.py
+++ learncone/ConeEstimatorSVM.py
@@ -38,43 +38,47 @@ class ConeEstimatorSVM(BaseEstimator):
             self.C = params['C']
         return self
 
-    def fit(self, data, class_values):
+    def fit(self, data, original_class_values):
         logging.info("Starting learning from %d data points",
                      len(data))
-        if len(set(class_values)) != 2:
+        if len(set(original_class_values)) != 2:
             raise ValueError('Need exactly two class values.')
         original_data = data
-        original_class_values = class_values
+        self.original_class_values = original_class_values
+        self.class_values = np.copy(original_class_values)
 
-        self.positive_class = max(class_values)
+        self.positive_class = max(self.class_values)
         self.svcs = []
-        predictions = np.array([self.positive_class]*len(class_values))
-        scores = []
+        self.predictions = np.array([self.positive_class]*len(self.class_values))
+        self.scores = []
         for i in range(self.max_dimensions):
-            if len(set(class_values)) != 2:
+            if len(set(self.class_values)) != 2:
                 logging.info("Only one class value remains, terminating learning")
                 break
             logging.info("Starting learning iteration %d, data points %d",
-                         i, len(class_values))
+                         i, len(self.class_values))
             svc = RecallSVMEstimator(self.beta, C=self.C)
-            svc.fit(data, class_values)
+            svc.fit(data, self.class_values)
             self.svcs.append(svc)
 
-            new_predictions = svc.predict(original_data)
-            predictions = np.minimum(predictions, new_predictions)
-            scores.append(f1_score(original_class_values, predictions))
-
-            judgments = svc.predict(data)
-            positives = class_values == self.positive_class
-            incorrect_negatives = ((class_values != self.positive_class)
-                                   & (judgments != class_values))
-            new_indices = positives | incorrect_negatives
+            new_indices = self.get_new_indices(original_data, data, svc)
             data = data[new_indices]
-            class_values = class_values[new_indices]
-        logging.info("F1 scores for iterations: %s", str(scores))
-        best_index = np.argmax(scores)
+            self.class_values = self.class_values[new_indices]
+        logging.info("F1 scores for iterations: %s", str(self.scores))
+        best_index = np.argmax(self.scores)
         self.svcs = self.svcs[:best_index + 1]
         logging.info("Number of SVM classifiers kept: %d", len(self.svcs))
+
+    def get_new_indices(self, original_data, data, svc):
+        new_predictions = svc.predict(original_data)
+        self.predictions = np.minimum(self.predictions, new_predictions)
+        self.scores.append(f1_score(self.original_class_values, self.predictions))
+
+        judgments = svc.predict(data)
+        positives = self.class_values == self.positive_class
+        incorrect_negatives = ((self.class_values != self.positive_class)
+                               & (judgments != self.class_values))
+        return positives | incorrect_negatives        
 
     def predict(self, data):
         predictions = np.array([self.positive_class]*len(data))
